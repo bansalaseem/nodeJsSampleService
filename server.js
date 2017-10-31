211da111b275df67eb439b914208239484ed122b
Jitendra Kumar <jikuma@microsoft.com>
2017-10-31 10:46:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello DockerHub 9\n');
+  res.send('Hello DockerHub 10\n');
 });
 
 app.listen(PORT, HOST);
